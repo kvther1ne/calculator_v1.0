90878790904064561771410e07351def8d5a67e8
Ekaterina Tabunshchikova <57044882+kvther1ne@users.noreply.github.com>
2023-05-04 11:56:22 +0300

    Add files via upload

---
+++ app/CMakeFiles/SmartCalc.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/calc.c" "CMakeFiles/SmartCalc.dir/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/calc.c.o" "gcc" "CMakeFiles/SmartCalc.dir/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/calc.c.o.d"
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/secondary.c" "CMakeFiles/SmartCalc.dir/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/secondary.c.o" "gcc" "CMakeFiles/SmartCalc.dir/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/secondary.c.o.d"
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/src/app/SmartCalc_autogen/mocs_compilation.cpp" "CMakeFiles/SmartCalc.dir/SmartCalc_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/SmartCalc_autogen/mocs_compilation.cpp.o.d"
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/qt/SmartCalc/main.cpp" "CMakeFiles/SmartCalc.dir/main.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/main.cpp.o.d"
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/qt/SmartCalc/qcustomplot.cpp" "CMakeFiles/SmartCalc.dir/qcustomplot.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/qcustomplot.cpp.o.d"
+  "/Users/vinitama/Desktop/C7_SmartCalc_v1.0-0/qt/SmartCalc/smartcalc.cpp" "CMakeFiles/SmartCalc.dir/smartcalc.cpp.o" "gcc" "CMakeFiles/SmartCalc.dir/smartcalc.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
